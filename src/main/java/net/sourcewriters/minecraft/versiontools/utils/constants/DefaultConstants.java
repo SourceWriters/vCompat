1f84b9caf7bc2b9f7ad6e80812d5604dbaf3847a
Laura Endreß <laura.endress@syntaxphoenix.com>
2020-11-08 14:14:16 +0100

    Fix namespace bc of syntaxapi

--- src/main/java/net/sourcewriters/minecraft/versiontools/utils/constants/DefaultConstants.java
+++ src/main/java/net/sourcewriters/minecraft/versiontools/utils/constants/DefaultConstants.java
@@ -1,10 +1,10 @@
 package net.sourcewriters.minecraft.versiontools.utils.constants;
 
-import com.syntaxphoenix.syntaxapi.data.key.Namespace;
+import com.syntaxphoenix.syntaxapi.utils.key.Namespace;
 
 public class DefaultConstants {
-	
+
 	public static final String NAMESPACE_STRING = "versionUtils";
-	public static final Namespace NAMESPACE = new Namespace(NAMESPACE_STRING);
+	public static final Namespace NAMESPACE = Namespace.of(NAMESPACE_STRING);
 
 }
