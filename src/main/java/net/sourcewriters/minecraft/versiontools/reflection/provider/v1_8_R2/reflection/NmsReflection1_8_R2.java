b1135e1d568634a100bfdbaa7fd38e7a9cb7d057
Laura Endreß <laura.endress@syntaxphoenix.com>
2021-01-17 02:53:02 +0100

    Added support for 1.8.3

---
+++ src/main/java/net/sourcewriters/minecraft/versiontools/reflection/provider/v1_8_R2/reflection/NmsReflection1_8_R2.java
@@ -0,0 +1,34 @@
+package net.sourcewriters.minecraft.versiontools.reflection.provider.v1_8_R2.reflection;
+
+import net.minecraft.server.v1_8_R2.NBTTagEnd;
+import net.minecraft.server.v1_8_R2.NBTTagList;
+import net.minecraft.server.v1_8_R2.PacketPlayOutPlayerListHeaderFooter;
+import net.sourcewriters.minecraft.versiontools.reflection.reflect.ReflectionProvider;
+import net.sourcewriters.minecraft.versiontools.reflection.reflect.Reflections;
+
+public class NmsReflection1_8_R2 extends Reflections {
+
+	public static NmsReflection1_8_R2 INSTANCE = new NmsReflection1_8_R2();
+
+	private NmsReflection1_8_R2() {}
+
+	@Override
+	public void setup(ReflectionProvider provider) {
+
+		//
+		//
+		// Create Reflects
+		//
+
+		//
+		// Minecraft
+
+		provider.createReflect("nmsPacketPlayOutPlayerListHeaderFooter", PacketPlayOutPlayerListHeaderFooter.class).searchField("header", "a")
+			.searchField("header", "b");
+		
+		provider.createReflect("nmsNBTTagEnd", NBTTagEnd.class);
+		provider.createReflect("nmsNBTTagList", NBTTagList.class).searchField("value", "list");
+
+	}
+
+}
