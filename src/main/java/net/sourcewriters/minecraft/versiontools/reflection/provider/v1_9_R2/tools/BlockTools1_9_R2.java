1ae790b9f6730b375d28e03722601df3d823756b
Laura Endreß <laura.endress@syntaxphoenix.com>
2021-01-17 02:33:02 +0100

    Added support for 1.9.4

---
+++ src/main/java/net/sourcewriters/minecraft/versiontools/reflection/provider/v1_9_R2/tools/BlockTools1_9_R2.java
@@ -0,0 +1,39 @@
+package net.sourcewriters.minecraft.versiontools.reflection.provider.v1_9_R2.tools;
+
+import org.bukkit.block.Block;
+import org.bukkit.craftbukkit.v1_9_R2.block.CraftSkull;
+
+import com.mojang.authlib.properties.Property;
+import com.mojang.authlib.properties.PropertyMap;
+import com.syntaxphoenix.syntaxapi.reflection.AbstractReflect;
+import com.syntaxphoenix.syntaxapi.reflection.Reflect;
+
+import net.minecraft.server.v1_9_R2.TileEntitySkull;
+import net.sourcewriters.minecraft.versiontools.reflection.tools.BlockTools;
+import net.sourcewriters.minecraft.versiontools.utils.constants.MinecraftConstants;
+
+public class BlockTools1_9_R2 extends BlockTools {
+
+	private final AbstractReflect craftEntityStateRef = new Reflect(CraftSkull.class).searchField("tileEntity", "tileEntity");
+
+	@Override
+	public void setHeadTexture(Block block, String texture) {
+		if (!(block instanceof CraftSkull)) {
+			return;
+		}
+		TileEntitySkull entitySkull = (TileEntitySkull) craftEntityStateRef.getFieldValue("tileEntity", block);
+		PropertyMap map = entitySkull.getGameProfile().getProperties();
+		map.removeAll("textures");
+		map.put("textures", new Property("textures", MinecraftConstants.TEXTURE_SIGNATURE, texture));
+	}
+
+	@Override
+	public String getHeadTexture(Block block) {
+		if (!(block instanceof CraftSkull)) {
+			return null;
+		}
+		TileEntitySkull entitySkull = (TileEntitySkull) craftEntityStateRef.getFieldValue("tileEntity", block);
+		return entitySkull.getGameProfile().getProperties().get("textures").iterator().next().getValue();
+	}
+
+}
---
+++ src/main/java/net/sourcewriters/minecraft/versiontools/reflection/provider/v1_9_R2/tools/ServerTools1_9_R2.java
@@ -0,0 +1,20 @@
+package net.sourcewriters.minecraft.versiontools.reflection.provider.v1_9_R2.tools;
+
+import org.bukkit.Bukkit;
+import org.bukkit.craftbukkit.v1_9_R2.CraftServer;
+
+import net.sourcewriters.minecraft.versiontools.reflection.tools.ServerTools;
+
+public class ServerTools1_9_R2 extends ServerTools {
+
+	@Override
+	public void setMotd(String text) {
+		((CraftServer) Bukkit.getServer()).getServer().setMotd(text);
+	}
+
+	@Override
+	public String getMotd() {
+		return ((CraftServer) Bukkit.getServer()).getServer().getMotd();
+	}
+
+}
