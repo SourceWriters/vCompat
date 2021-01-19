3686942862b6562572f9d25db4f2bf0dcad64453
Laura Endreß <laura.endress@syntaxphoenix.com>
2021-01-19 11:38:33 +0100

    Script is fixed now

--- scripts/build-spigot.sh
+++ scripts/build-spigot.sh
@@ -1,18 +1,17 @@
 #!/usr/bin/env bash
 echo "Downloading Spigot BuildTools..."
-var path = $1
-mkdir -p $path/libraries/spigot
-mkdir $path/spigotWork
-curl https://hub.spigotmc.org/jenkins/job/BuildTools/lastBuild/artifact/target/BuildTools.jar --output $path/spigotWork/BuildTools.jar
+mkdir -p $1/libraries/spigot
+mkdir $1/spigotWork
+curl https://hub.spigotmc.org/jenkins/job/BuildTools/lastBuild/artifact/target/BuildTools.jar --output $1/spigotWork/BuildTools.jar
 echo "Successfully downloaded BuildTools"
 
 echo "Building Spigot files..."
-versions=$path/data/versions.txt
+versions=$1/scripts/data/versions.txt
 while read line; do
   if [ -z "$line"]; then
     continue
   fi
   echo "Building Spigot " + $line
-  java -jar $path/spigotWork/BuildTools.jar --rev $line --outputDir ../libraries/spigot
+  java -jar $1/spigotWork/BuildTools.jar --rev $line --outputDir ../libraries/spigot
 done < $versions
 echo "Building complete!"
