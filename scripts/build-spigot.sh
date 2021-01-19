91721d22710ba836c5c61dcd28ffe83dbaa0d338
Laura Endreß <laura.endress@syntaxphoenix.com>
2021-01-19 11:23:10 +0100

    Update build script

--- scripts/build-spigot.sh
+++ scripts/build-spigot.sh
@@ -1,17 +1,18 @@
 #!/usr/bin/env bash
 echo "Downloading Spigot BuildTools..."
-mkdir -p ${{ github.workspace }}/libraries/spigot
-mkdir ${{ github.workspace }}/spigotWork
-curl https://hub.spigotmc.org/jenkins/job/BuildTools/lastBuild/artifact/target/BuildTools.jar --output ${{ github.workspace }}/spigotWork/BuildTools.jar
+var path = $1
+mkdir -p $path/libraries/spigot
+mkdir $path/spigotWork
+curl https://hub.spigotmc.org/jenkins/job/BuildTools/lastBuild/artifact/target/BuildTools.jar --output $path/spigotWork/BuildTools.jar
 echo "Successfully downloaded BuildTools"
 
 echo "Building Spigot files..."
-versions=${{ github.workspace }}/data/versions.txt
+versions=$path/data/versions.txt
 while read line; do
   if [ -z "$line"]; then
     continue
   fi
   echo "Building Spigot " + $line
-  java -jar ${{ github.workspace }}/spigotWork/BuildTools.jar --rev $line --outputDir ../libraries/spigot
+  java -jar $path/spigotWork/BuildTools.jar --rev $line --outputDir ../libraries/spigot
 done < $versions
 echo "Building complete!"
