76a5f467e801e07d2dfadbe967b1a25f7ab6961b
Lauriichan <laura.endress@syntaxphoenix.com>
2022-01-19 18:30:51 +0100

    Try to fix github build

--- scripts/download-spigot.sh
+++ scripts/download-spigot.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 mkdir -p $1/libraries/spigot
-echo "Downloading Spigot files..."
-versions=$1/scripts/data/versions.txt
+echo "Downloading Server files..."
+versions=$1/scripts/data/servers.txt
 while read line; do
   if [ -z "$line" ]; then
     continue
   fi
-  echo "Downloading Spigot $line"
+  echo "Downloading Server $line"
   curl "http://test.playuniverse.org/host/download/github/spigot-$line.jar"  --output "$1/libraries/spigot/spigot-$line.jar"
 done < $versions
 echo "Download complete!"
